9f458be2b3a03be3cadd7b35544afbdd254c27f4
Christian Valencia <cvale069@fiu.edu>
2020-09-24 18:39:23 -0400

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Christian Valencia on 9/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
