61d40b9382a32d12418c43f3e50a5e2283ae0488
Christian Valencia <cvale069@fiu.edu>
2020-10-02 19:01:37 -0400

    Added superhero collections view

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Christian Valencia on 10/1/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
